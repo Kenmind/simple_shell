6937e67c3efc9baaf9208c49328ba08f637580d3
llwyd1 <llwydmodes@gmail.com>
2021-10-25 19:39:40 +0200

    Added function prototypes

--- shell.h
+++ shell.h
@@ -7,4 +7,8 @@
 #include <string.h>
 #include <stdarg.h>
 
+char *strcat(char *dest, const char *src);
+int strcmp(const char *s1, const char *s2);
+char *strtok(char *str, const char *delim);
+
 #endif
